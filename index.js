e95a47e3d463e10191856cc50fe1bfe7e4b04c60
Knapeto <knapeto@gmail.com>
2019-03-21 09:10:10 +0100

    ....

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+var chalk = require('chalk');
+var proxy = express();
+
+var startProxy = function(port, proxyUrl, proxyPartial) {
+  proxy.use(cors());
+  proxy.options('*', cors());
+
+  // remove trailing slash
+  var cleanProxyUrl = proxyUrl.replace(/\/$/, '');
+  // remove all forward slashes
+  var cleanProxyPartial = proxyPartial.replace(/\//g, '');
+
+  proxy.use('/' + cleanProxyPartial, function(req, res) {
+    try {
+      console.log(chalk.green('Request Proxied -> ' + req.url));
+    } catch (e) {}
+    req.pipe(request(cleanProxyUrl + req.url)).pipe(res);
+  });
+
+  proxy.listen(port);
+
+  // Welcome Message
+  console.log(chalk.bgGreen.black.bold.underline('\n Proxy Active \n'));
+  console.log(chalk.blue('Proxy Url: ' + chalk.green(cleanProxyUrl)));
+  console.log(chalk.blue('Proxy Partial: ' + chalk.green(cleanProxyPartial)));
+  console.log(chalk.blue('PORT: ' + chalk.green(port) + '\n'));
+  console.log(
+    chalk.cyan(
+      'To start using the proxy simply replace the proxied part of your url with: ' +
+        chalk.bold('http://localhost:' + port + '/' + cleanProxyPartial + '\n')
+    )
+  );
+};
+
+startProxy(8010, 'https://helsana-trails.coservice.ch', '/proxy');
